64b317ba4d4a4e4642d2b18379ddd201eba6502e
416135826 <abelmartin50@gmail.com>
2018-12-03 01:22:51 -0600

    Initial commit

---
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -0,0 +1,37 @@
+package unam.fca.dmoviles;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static int IMAGE_WIDTH = 400;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Actividad 8 Creacion de una calculadora de porcentajes
+        //En esta actividad crearas una aplicacion que calcule la diferencia relativa en porcentaje entre dos numeros.
+
+
+        // El archivo del layout de la actividad se encuentra en la carpeta
+        // res/layout/activity_main.xml
+        setContentView(R.layout.activity_main);
+
+
+
+        //Actividades
+        //a) Ejecuta la aplicacion y familiarizate con el layout definido en R.layout.activity_main
+        //b) Crea la logica para calcular la diferencia relativa en porcentaje entre dos numeros
+        //c) La formula para calcular la diferencia relativa en porcentaje esta en: https://www.calculatorsoup.com/calculators/algebra/percent-change-calculator.php
+        //d) La aplicacion debe calcular la diferencia relativa en porcentaje de los valores contenidos en "editText" y "editText2"
+        //e) Al hacer click en el boton "button", la aplicacion debera calcular dicha diferencia y poner el resultado en el campo "textViewResult"
+        //f) La aplicacion debera manejar adecuadamente el cambio de rotacion para el resultado y los campos de texto
+        //e) Anota tus respuestas y las capturas de pantalla en un documento en Word
+        //f) Sube tu codigo al repositorio.
+        //g) Sube el documento Word a la plataforma Moodle. Incluye la liga a tu repositorio
+
+
+    }
+}
